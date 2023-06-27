f96ee1314894b2a2f6a26e904209996f9aefebf6
Arrzed <83354828+Arrzed@users.noreply.github.com>
2023-06-27 15:32:21 -0300

    Add files via upload

---
+++ polygon.py
@@ -0,0 +1,52 @@
+class Rectangle:
+
+    def __init__(self,wid,hei):
+        self.width = wid
+        self.height = hei
+
+    def __str__(self):
+        return 'Rectangle(width=' + str(self.width) + ', height=' + str(self.height) + ')'
+
+    def set_width(self,wid):
+        self.width = wid
+
+    def set_height(self,hei):
+        self.height = hei
+
+    def get_area(self):
+        return self.width * self.height
+
+    def get_perimeter(self):
+        return (2 * self.width) + (2 * self.height)
+
+    def get_diagonal(self):
+        return (self.width ** 2 + self.height ** 2) ** .5
+
+    def get_picture(self):
+        if self.width > 50 or self.height > 50:
+            return 'Too big for picture.'
+        count = self.height
+        picture = ''
+        while count > 0:
+            picture += '*' * self.width + '\n'
+            count = count - 1
+        return picture
+
+    def get_amount_inside(self,shape):
+        total = self.width * self.height
+        rst = shape.width * shape.height
+        return int(total / rst)
+
+class Square(Rectangle):
+
+    def __init__(self,side):
+        self.width = side
+        self.height = side
+
+    def __str__(self):
+        return 'Square(side=' + str(self.width) + ')'
+
+    def set_side(self,number):
+        self.width = number
+        self.height = number
+
---
+++ readme.txt
@@ -0,0 +1,14 @@
+Use of object oriented programming to create a Rectangle class and a Square class. The Square class is a subclass of Rectangle and inherit methods and attributes.
+
+Methods:
+
+set_width
+set_height
+get_area
+get_perimeter
+get_diagonal
+get_picture: Returns a string that represents the shape using lines of "*"
+get_amount_inside: Takes another shape (square or rectangle) as an argument. Returns the number of times the passed in shape could fit inside the shape (with no rotations)
+
+Square exclusive method:
+set_side
